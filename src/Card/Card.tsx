9a72d0a0b0d1658a86ae4206850b4c52f006a7ba
vlbychkov <vladislav.bychkov01@gmail.com>
2022-08-27 19:46:42 +0300

    add styles

--- src/Card/Card.tsx
+++ src/Card/Card.tsx
@@ -5,7 +5,7 @@ const CardStyled = styled.form`
   	position: absolute;
   	width: 26%;
   	height: 29%;
-  	background: #0A2973;
+  	background: #242424;
   	box-shadow: 0px 2px 20px 1px rgba(0, 0, 0, 0.3);
   	border-radius: 25px;
   	margin-bottom: 30%;
