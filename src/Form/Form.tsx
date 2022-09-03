6a52fc6891a922b1a040327ceb0ff3d3bfbd25e5
vlbychkov <vladislav.bychkov01@gmail.com>
2022-09-03 15:27:10 +0300

    add fix

--- src/Form/Form.tsx
+++ src/Form/Form.tsx
@@ -1,7 +1,7 @@
 import styled from "styled-components";
-import Input from "./Input/Input";
 import Card from "../Card/Card";
-// import Label from "./Label/Label";
+import React from "react";
+import Input from "./Input/Input";
 
 const FormStyled = styled.form`
   display: grid;
@@ -11,10 +11,9 @@ const FormStyled = styled.form`
   z-index: 2;
   width: 46%;
   height: 57%;
-  background: #FFFFFF;
+  background: #ffffff;
   box-shadow: 0 2px 20px 1px rgba(0, 0, 0, 0.25);
   border-radius: 25px;
-
 
   @media (max-width: 1024px) {
     width: 70%;
@@ -27,7 +26,7 @@ const FormStyled = styled.form`
     height: 70%;
     margin-top: 4em;
   }
-`
+`;
 
 const WrapperBlockInputs = styled.div`
   grid-row: 2 / 5;
@@ -39,37 +38,41 @@ const WrapperBlockInputs = styled.div`
   height: 100%;
   margin-top: -0.5em;
   padding-right: 0.6em;
-
-`
+`;
 const FieldInput = styled.div`
   width: 55.5%;
+  height: 100%;
   display: flex;
   flex-direction: column;
-  height: 100px;
-  justify-content: space-around;
-`
+  justify-content: space-evenly;
+  margin-top: 2em;
+  margin-bottom: 2em;
+`;
+
 
 
 const Form = () => {
-    return <FormStyled>
-        <Card/>
-        <WrapperBlockInputs>
-            <FieldInput>
-                <Input type={"text"} width={'100%'} model={'numberCard'}/>
-                <div
-                    style={{
-                        display: "flex",
-                        justifyContent: 'space-between',
-                        height: '100px',
-                        width: "100%"
-                    }}>
-                    <Input type={"text"} width={'100%'} model={'date'}/>
-                    <Input type={'text'} width={'100%'} model={'cvc'}/>
-                </div>
-                <Input type={"text"} width={'100%'} model={'name'}/>
-            </FieldInput>
-        </WrapperBlockInputs>
+  return (
+    <FormStyled>
+      <Card />
+      <WrapperBlockInputs>
+        <FieldInput>
+          <Input width={"100%"} model={"numberCard"} />
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "space-between",
+              width: "100%",
+            }}
+          >
+            <Input width={'30%'} model={'date'}/>
+            <Input width={'30%'} model={'cvc'}/>
+          </div>
+          <Input width={'100%'} model={'name'}/>
+        </FieldInput>
+      </WrapperBlockInputs>
     </FormStyled>
-}
+  );
+};
 
 export default Form;
--- src/Form/Input/Input.tsx
+++ src/Form/Input/Input.tsx
@@ -1,94 +1,84 @@
 import styled from "styled-components";
 import React from "react";
-import {IMask, IMaskMixin} from 'react-imask';
+import {IMask, IMaskMixin} from "react-imask";
 
-
-interface InputProps {
-    type: string
-    width: string
-    model: string
+interface PropsStyle {
+  widthInput: string;
 }
 
-const InputStyled = styled.input`
-  width: ${props => props.width};
-  background: #F3F3F3;
-  box-shadow: 0px 2px 7px rgba(0, 0, 0, 0.25);
+const InputStyled = styled.input<PropsStyle>`
+  width: ${(props) => props.widthInput};
+  height: 50px;
+  background: #f3f3f3;
+  box-shadow: 0 2px 7px rgba(0, 0, 0, 0.25);
   border-radius: 5px;
   border: none;
-  height: 50px;
   font-size: 28px;
   color: #434343;
   padding-left: 0.4em;
   margin-right: -0.4em;
-`
+`;
 
-const MaskedStyledInput = IMaskMixin(({...props}) => (
-    <InputStyled
-        width={props.width}
-        ref={props.inputRef}
-        placeholder={props.placeholder}
-        // onChange={(e)=> {console.log(e.currentTarget.value)} }
+const MaskedInput = IMaskMixin(({ ...props }: any) => {
+  return <InputStyled ref={props.inputRef} widthInput={props.width} placeholder={props.placeholder}/>;
+});
 
-    />
-));
 
-const Input: React.FC<InputProps> = ({type, width, model}) => {
-    
-    const maskSwitch = (model: string) => {
-        switch (model) {
-            case 'numberCard':
-                return "0000 0000 0000 0000"
-            case 'date':
-                return "MM / YY"
-            case 'cvc':
-                return '0000'
-            case 'name':
-                return /^[A-Z .]{1,20}$/
-            default:
-                return ''
-        }
+const Input = ({ width, model }: { model: string; width: string }) => {
+  const date: any = new Date();
+  const dateLast2Number: string = date.getFullYear().toString().substr(2, 4);
+  
+  const maskSwitch = (model: string) => {
+    switch (model) {
+      case "numberCard":
+        return "0000 0000 0000 0000";
+      case "date":
+        return "MM / YY";
+      case "cvc":
+        return "0000";
+      case "name":
+        return /^[A-Z .]{1,20}$/;
+      default:
+        return "";
     }
-    
-    const placeholderSwitch = (model: string) => {
-        switch (model) {
-            case 'numberCard':
-                return "Card Number"
-            case 'date':
-                return "MM / YY"
-            case 'cvc':
-                return 'CVC / CVV'
-            case 'name':
-                return 'IVAN IVANOV'
-            default:
-                return ''
-        }
+  };
+  
+  const placeholderSwitch = (model: string) => {
+    switch (model) {
+      case "numberCard":
+        return "Card Number";
+      case "date":
+        return "MM / YY";
+      case "cvc":
+        return "CVC / CVV";
+      case "name":
+        return "IVAN IVANOV";
+      default:
+        return "";
     }
-    const date: any = new Date()
-    const dateLast2Number:string = date.getFullYear().toString().substr(2, 4)
-    // @ts-ignore
-    return <MaskedStyledInput
-        width={width}
-        mask={maskSwitch(model)}
-        placeholder={placeholderSwitch(model)}
-        prepare={
-            function (str: string) {
-                return str.toUpperCase();
-            }
-        }
-        blocks={{
-        	YY: {
-                mask: IMask.MaskedRange,
-                from: +dateLast2Number,
-                to: +dateLast2Number + 15,
-        	},
-        	MM: {
-        		mask: IMask.MaskedRange,
-        		from: 1,
-        		to: 12
-        	}
-        }}
-    />
-}
-
+  };
+  // @ts-ignore
+  // Заглушка нужна для того, чтобы не ругался на Regexp
+  return <MaskedInput
+      width={width}
+      mask={maskSwitch(model)}
+      placeholder={placeholderSwitch(model)}
+      prepare={function (str: string) {
+        return str.toUpperCase();
+      }}
+      blocks={{
+        YY: {
+          mask: IMask.MaskedRange,
+          from: +dateLast2Number,
+          to: +dateLast2Number + 15,
+        },
+        MM: {
+          mask: IMask.MaskedRange,
+          from: 1,
+          to: 12,
+        },
+      }}
+  />
+};
 
 export default Input;
